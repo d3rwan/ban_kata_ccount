9fe9edb6a1edb2b1c2ddd52558e99818de077517
d3rwan <erwan.deruelle@gmail.com>
2016-09-11 11:44:40 +0200

    Initialisation du projet

---
+++ src/main/java/fr/carbonit/kata/bankaccount/BankAccountKata.java
@@ -0,0 +1,17 @@
+package fr.carbonit.kata.bankaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class BankAccountKata {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(BankAccountKata.class, args);
+    }
+
+}
